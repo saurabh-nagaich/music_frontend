2876385eeff623bbae1c806ebc34e0e20f87bd78
unknown <saurabhnagaich858@gmail.com>
2020-11-10 10:17:44 +0530

    media

--- src/component/main/Sign_up.js
+++ src/component/main/Sign_up.js
@@ -16,16 +16,33 @@ function Sign_up() {
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState()
     
+    const [sign_page,setPage] = useState("sign_up")
     // console.log(userName,email,password)
 
     const sign_up = () =>{
-        const obj={
-            userName ,
-            email,
-            password
+        if(userName && password && email){
+            const obj={
+                userName ,
+                email,
+                password
+            }
+            console.log(obj)
+    
         }
-        console.log(obj)
-
+        setUserName(()=>"");
+        setEmail(()=>"");
+        setPassword(()=>null)
+    }
+    const sign_In = () =>{
+        if((userName||email) && password){
+            const obj={
+                userName ,
+                email,
+                password
+            }
+            console.log(obj)
+    
+        }
         setUserName(()=>"");
         setEmail(()=>"");
         setPassword(()=>null)
@@ -41,8 +58,7 @@ function Sign_up() {
                     <img src={imageLink} id="connect_image" />
                 </div>
                 <div className="Sign_up_right">
-                    <div className="sign_up_box">
-
+                    <div className={`sign_up_box ${sign_page}` }>
                         <div className="sign_up_heading">Sign Up</div>
                         <div class="input ">
                             <input class="input_area" type="text" placeholder="" onChange={e=>setUserName(e.target.value)} />
@@ -72,13 +88,37 @@ function Sign_up() {
                             </div>
                         </div>
                         <div className="signUp_btn" onClick={sign_up} >Sign up</div>
-                        <div className="login_link_line">Already have an account ? <span className="log">Log in</span></div>
+                        <div className="login_link_line">Already have an account ? <span className="log" onClick={()=>sign_page==="sign_in"?setPage(""):setPage("sign_in")}>Log in</span></div>
                         <div className="sign_up_bottom">
                             <hr />
                             <div className="or">or</div>
                             <hr />
                         </div>
                     </div>
+                    <div className={`sign_in_box  ${sign_page === "sign_up"?null:"login_page"}`}>
+                        <div className="sign_up_heading">Sign In</div>
+                        <div class="input ">
+                            <input class="input_area" type="text" placeholder="" onChange={e=>setUserName(e.target.value)} />
+                            <div >
+                                <span class="icon">
+                                        <SupervisorAccountIcon id="icon"/>
+                                </span>
+                                <label className={`label ${userName?"stop":null} `}>User Name</label>
+                            </div>
+                        </div>
+                        <div class="input ">
+                            <input class="input_area" type="text" placeholder="" onChange={e=>setPassword(e.target.value)} />
+                            <div >
+                                <span class="icon">
+                                    <VpnKeyOutlinedIcon id="icon" />
+                                </span>
+                                <label className={`label ${userName?"stop":null} `}>Password</label>
+                            </div>
+                        </div>
+                        <div className="signUp_btn" onClick={sign_In} >Sign In</div>
+                        <div className="login_link_line">create new account ? <span className="log" onClick={()=>sign_page==="sign_up"?setPage(""):setPage("sign_up")}>Sign Up</span></div>                            
+                    </div>
+                    
                 </div>
             </div>
         </div>
